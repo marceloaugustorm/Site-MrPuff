da3baffc1a7dc34cb2d5f3f77a4c038654ddcfe7
marceloaugustorm <marcelo0204martins@gmail.com>
2025-03-21 00:23:55 -0300

    Subindo pra Vercel

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -6,8 +6,14 @@ import Rosa from "../assets/Rosa e Azul.jpeg";
 function Body(){
     return(
         <div>
-            <h2  className='cardapio'>Veja nosso cardápio</h2>
+            <a href="https://mrpuff-drinks-o-hookah-o-culture.goomer.app/menu">
+            <h2  className='cardapio'>Veja nosso cardápio </h2>
+            </a>
+
+            <a href="https://wa.me/message/46HCAK57GAZAC1?fbclid=PAZXh0bgNhZW0CMTEAAaYa0fJiQF57zalgo7imqq_mphmbysflQ0PeUWxD5DYn8G6DXzAzNrLz8xY_aem_HglLUdjBJ5g9pWqzXli1Vg.">
             <h2 className='pedido'>Faça aqui seu pedido</h2>
+            </a>
+
             <h2 className='produtos' >Nossos produtos</h2>
             <div className="corpo">
                 <img className='fundo_body' src={Rosa} alt="" />
