5b549da5cfd6efaf1e4cc68234a013f544cc7ee2
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-08-06 10:21:51 -0300

    Remove chave Google API e usa variável de ambiente

--- server.cjs
+++ server.cjs
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const fetch = require('node-fetch');
 const express = require('express');
 const cors = require('cors');
@@ -7,27 +8,22 @@ const port = 3001;
 
 app.use(cors());
 
-const apiKey = "AIzaSyBh040GemOGy4FzcApEEXyJOTZL0k-I0X4";
-const placeId = "ChIJxcp-xFFpzpQRRdtbr-jwub8";
+const apiKey = process.env.GOOGLE_API_KEY;
+const placeId = process.env.PLACE_ID;
 
-app.get("/reviews", async (req, res)=>{
-
-    try{
+app.get("/reviews", async (req, res) => {
+    try {
         const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,reviews&key=${apiKey}`;
-        const response = await fetch(url)
+        const response = await fetch(url);
+        const data = await response.json();
 
-        const data = await response.json()
-
-        res.json(data.result.reviews);
-
-
-    }catch(error){
-        console.log("Erro", error.message)
-        res.status(500).json({"erro": response.message});
+        res.json(data.result?.reviews || []);
+    } catch (error) {
+        console.error("Erro", error.message);
+        res.status(500).json({ erro: error.message });
     }
-}
-)
+});
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
