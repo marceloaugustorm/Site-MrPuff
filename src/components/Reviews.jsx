4b8b69a1f0eb872735111fe3b97b6f8683978f4f
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-08-07 16:00:59 -0300

    consumo api atualizado

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -10,7 +10,7 @@ const Reviews = () => {
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/reviews');
+        const response = await fetch('https://consumo-api-z8t2.onrender.com/reviews/reviews');
         const data = await response.json();
         setReviews(data);
       } catch (error) {
